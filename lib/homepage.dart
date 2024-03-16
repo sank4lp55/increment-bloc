a8f650fddbff02e7a0205858612fe309987465f8
sank4lp55 <sankalpsrivastav55@gmail.com>
2024-03-16 21:56:56 +0530

    UI init

---
+++ lib/homepage.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Counter App")),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            Container(
+              child: const Text(
+                "1",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                    decoration: BoxDecoration(
+                        color: Colors.purple[100],
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 1.0), //(x,y)
+                            blurRadius: 7.0,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(6)),
+                    child: Text(
+                      "Decrement",
+                      style: TextStyle(
+                          color: Colors.purple[400],
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.5),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                    decoration: BoxDecoration(
+                        color: Colors.purple[100],
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 1.0), //(x,y)
+                            blurRadius: 7.0,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(6)),
+                    child: Text(
+                      "Increment",
+                      style: TextStyle(
+                          color: Colors.purple[400],
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.5),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
